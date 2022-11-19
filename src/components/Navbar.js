682b4779264b1ba340d87adde081d22515e92b4f
Sajeel Zafar <sajeelzafar1995@gmail.com>
2022-11-19 09:48:13 +0500

    Navbar styling complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
   const links = [
     {
       id: 1,
       path: '/books',
-      text: 'Books',
+      text: 'BOOKS',
     },
     {
       id: 2,
       path: '/categories',
-      text: 'Categories',
+      text: 'CATEGORIES',
     },
   ];
   return (
-    <nav>
-      <h1>
-        <Link to="/"> Bookstore CMS</Link>
-      </h1>
-      <ul>
-        {links.map((link) => (
-          <li key={link.id}>
-            <Link to={link.path}>{link.text}</Link>
-          </li>
-        ))}
-      </ul>
+    <nav className="navBar">
+      <div className="navLeftSide">
+        <h1>
+          <Link className="navBarHeading" to="/"> Bookstore CMS</Link>
+        </h1>
+        <ul className="navBarLinks">
+          {links.map((link) => (
+            <li key={link.id}>
+              <Link className="navBarItem" to={link.path}>{link.text}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="Oval">
+        {' '}
+        <FontAwesomeIcon className="userIcon" icon={faUser} />
+      </div>
+
     </nav>
   );
 };
